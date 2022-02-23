0287dfab277a74c37b653071527b460c654ce90f
asadozzaman <asadozzaman278061@gmail.com>
2022-02-23 11:46:40 +0600

    Add to cart functional view

--- order/urls.py
+++ order/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from store import views
+from order import views
 
 app_name = 'order'
 
 
 urlpatterns = [
+    path('add-to-cart/<int:pk>/', views.add_to_cart, name='add-to-cart'),
 
 ]
 
