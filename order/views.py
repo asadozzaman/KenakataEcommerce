2833229daad0b884feca28b38ac68243b00bc999
asadozzaman <asadozzaman278061@gmail.com>
2022-02-24 12:30:43 +0600

    Increase & decrease quantity

--- order/views.py
+++ order/views.py
@@ -63,5 +63,41 @@ def remove_item_from_cart(request, pk):
     else:
         return redirect('order:cart')
 
+def increase_cart(request, pk):
+    item = get_object_or_404(Product, pk=pk)
+    order_qs = Order.objects.filter(user=request.user, ordered=False)
+    if order_qs.exists():
+        order = order_qs[0]
+        if order.orderitems.filter(item=item).exists():
+            order_item = Cart.objects.filter(item=item, user=request.user, purchased=False)[0]
+            if order_item.quantity >= 1:
+                order_item.quantity += 1
+                order_item.save()
+                return redirect('order:cart')
+        else:
+            return redirect('store:index')
+    else:
+        return redirect('store:index')
+
+
+def decrease_cart(request, pk):
+    item = get_object_or_404(Product, pk=pk)
+    order_qs = Order.objects.filter(user=request.user, ordered=False)
+    if order_qs.exists():
+        order = order_qs[0]
+        if order.orderitems.filter(item=item).exists():
+            order_item = Cart.objects.filter(item=item, user=request.user, purchased=False)[0]
+            if order_item.quantity > 1:
+                order_item.quantity -= 1
+                order_item.save()
+                return redirect("order:cart")
+            else:
+                order.orderitems.remove(order_item)
+                order_item.delete()
+                return redirect('store:index')
+        else:
+            return redirect('store:index')
+    else:
+        return redirect('store:index')
 
 
