b19129904ed8fc1e2ddadc4380e9cea7c50e3e4e
asadozzaman <asadozzaman278061@gmail.com>
2022-02-27 23:08:12 +0600

    Simple product search program

--- store/views.py
+++ store/views.py
@@ -1,17 +1,29 @@
 from django.shortcuts import render
-from  django.views.generic import ListView,DetailView
+from  django.views.generic import ListView,DetailView,TemplateView
 from store.models import Category,Product,ProductImages,Banner
 # Create your views here.
 
-class HomeListView(ListView):
-    model = Product
-    template_name = 'store/index.html'
-    context_object_name = 'products'
+class HomeListView(TemplateView):
+    def get(self, request, *args, **kwargs):
+        products = Product.objects.all().order_by('-id')
+        banners = Banner.objects.filter(is_active=True).order_by('-id')[0:3]
 
-    def get_context_data(self, *args, **kwargs):
-        context = super().get_context_data(*args,**kwargs)
-        context['banners'] = Banner.objects.filter(is_active=True).order_by('-id')[1:3]
-        return context
+        context = {
+            'products': products,
+            'banners': banners
+        }
+        return render(request, 'store/index.html', context)
+
+    def post(self, request, *args, **kwargs):
+        if request.method == 'post' or request.method == 'POST':
+            search_product = request.POST.get('search_product')
+            products = Product.objects.filter(name__icontains=search_product).order_by('-id')
+
+            context = {
+                'products': products
+            }
+            return render(request, 'store/index.html', context)
+
 
 
 # def home(request):
