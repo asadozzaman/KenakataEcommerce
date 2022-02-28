f552b25d5c54ddfafad8ad85ca8ea47d61838119
asadozzaman <asadozzaman278061@gmail.com>
2022-02-28 09:02:05 +0600

    Fixed some errors

--- order/views.py
+++ order/views.py
@@ -7,38 +7,39 @@ from coupon.models import Coupon
 from coupon.forms import CouponCodeForm
 # Create your views here.
 
-def add_to_cart(request,pk):
-    item =  get_object_or_404(Product,pk=pk)
-    order_item = Cart.objects.get_or_create(item=item,user=request.user,purchased = False)
-    order_qs = Order.objects.filter(user=request.user,ordered = False)
-    if order_qs.exists():
-        order = order_qs[0]
-        if order.orderitems.filter(item=item).exists():
-            size = request.POST.get('size')
-            color = request.POST.get('color')
-            quantity = request.POST.get('quantity')
-            print('iffffff'+color)
-            if quantity:
-                order_item[0].quantity += int(quantity)
+def add_to_cart(request, pk):
+    if request.user.is_authenticated:
+        item = get_object_or_404(Product, pk=pk)
+        order_item = Cart.objects.get_or_create(item=item, user=request.user, purchased=False)
+        order_qs = Order.objects.filter(user=request.user, ordered=False)
+        if order_qs.exists():
+            order = order_qs[0]
+            if order.orderitems.filter(item=item).exists():
+                size = request.POST.get('size')
+                color = request.POST.get('color')
+                quantity = request.POST.get('quantity')
+                if quantity:
+                    order_item[0].quantity += int(quantity)
+                else:
+                    order_item[0].quantity += 1
+                order_item[0].size = size
+                order_item[0].color = color
+                order_item[0].save()
+                return redirect('store:index')
             else:
-                order_item[0].quantity += 1
-            order_item[0].size = size
-            order_item[0].color = color
-            order_item[0].save()
-            return redirect('store:index')
+                size = request.POST.get('size')
+                color = request.POST.get('color')
+                order_item[0].size = size
+                order_item[0].color = color
+                order.orderitems.add(order_item[0])
+                return redirect('store:index')
         else:
-            size = request.POST.get('size')
-            color = request.POST.get('color')
-            print('elseeeeeeeee' + color)
-            order_item[0].size = size
-            order_item[0].color = color
+            order = Order(user=request.user)
+            order.save()
             order.orderitems.add(order_item[0])
             return redirect('store:index')
     else:
-        order = Order(user=request.user)
-        order.save()
-        order.orderitems.add(order_item[0])
-        return redirect('store:index')
+        return redirect('account:login')
 
 def cart_view(request):
     if request.user.is_authenticated:
